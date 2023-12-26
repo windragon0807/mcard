d120cf721ab947a2c53b694e0887fb606cd3b494
Ryong <tmdfyd1995@gmail.com>
2023-12-26 17:58:27 +0900

    Emotion 설정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import logo from './logo.svg';
 import './App.css';
 
+const bold2 = 'bold';
+
+const bold = css`
+  font-weight: ${bold2};
+`;
+
+const containerStyle = css`
+  background-color: pink;
+  ${bold};
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+  ${bold};
+`;
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={containerStyle}>
+      <Button>스타일버튼</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
