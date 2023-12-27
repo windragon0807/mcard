2709f9355b1de0628a119e7c297da246a5733703
Ryong <tmdfyd1995@gmail.com>
2023-12-28 01:00:25 +0900

    공통 컴포넌트 구현

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalStyles';
+
+import { AlertContextProvider } from '@contexts/AlertContext';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Global styles={globalStyles} />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
